8054c8c75e25a5dc4c5eaf4f093fbb5e90e97777
farewell12345 <1149558764@qq.com>
2022-03-29 16:58:25 +0800

    webhook.py

--- webhook.py
+++ webhook.py
@@ -17,12 +17,18 @@ def encryption(data):
 import os
 
 
-@app.route('/hook', methods=['POST','GET'])
+@app.route('/hook', methods=['POST'])
 def post_data():
     """
     github加密是将post提交的data和WebHooks的secret通过hmac的sha1加密，放到HTTP headers的
     X-Hub-Signature参数中
     """
+    post_data = request.data
+    token = encryption(post_data)
+    # 认证签名是否有效
+    signature = request.headers.get('X-Hub-Signature', '').split('=')[-1]
+    if signature != token:
+        return "token认证无效", 401
     # 运行shell脚本，更新代码
     os.system('sh deploy.sh')
     return jsonify({"status": 200})
