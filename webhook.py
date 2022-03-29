b3051296b044bd700986883553dc5f57450f8558
farewell12345 <1149558764@qq.com>
2022-03-29 17:09:54 +0800

    webhook.py

--- webhook.py
+++ webhook.py
@@ -4,7 +4,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 # github中webhooks的secret
-github_secret = 'xxxxxxxx'
+github_secret = 'imfFTJq4'
 
 
 def encryption(data):
@@ -16,8 +16,6 @@ def encryption(data):
 # webhook.py
 import os
 
-token = 'imfFTJq4'
-
 
 @app.route('/hook', methods=['POST'])
 def post_data():
@@ -25,9 +23,10 @@ def post_data():
     github加密是将post提交的data和WebHooks的secret通过hmac的sha1加密，放到HTTP headers的
     X-Hub-Signature参数中
     """
+    post_data = request.data
+    token = encryption(post_data)
     # 认证签名是否有效
-    signature = request.headers.get('X-Hub-Signature')
-    print("-------signature:"+signature+"-----------")
+    signature = request.headers.get('X-Hub-Signature', '').split('=')[-1]
     if signature != token:
         return "token认证无效", 401
     # 运行shell脚本，更新代码
@@ -36,4 +35,4 @@ def post_data():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0',port=8000)
