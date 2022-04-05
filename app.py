68a1bfaca69a69f39d8f31e097faad5d48e7c91f
farewell12345 <1149558764@qq.com>
2022-04-05 17:11:05 +0800

    update

--- app.py
+++ app.py
@@ -134,7 +134,6 @@ def cors_middle_res(request: Request, response: HTTPResponse):
 下面为一个简易的服务器端，可快速部署图像处理服务
 '''
 
-
 @app.route("/hello", methods=['GET', 'POST'])
 async def he(request):
     return text("hello world")
